7c955b02ee9d174f4386ccfad37fce6095372cec
whatwewant <tobewhatwewant@gmail.com>
2019-08-28 09:08:54 +0800

    :bug:(parse): fix parse find all indexes failed

--- src/parse.ts
+++ src/parse.ts
@@ -1,13 +1,16 @@
 import { Word, WordWithPosition } from './interface';
 
 export function parse<Args>(text: string, words: Word<Args>[]): WordWithPosition<Args>[] {
-  return words.map(word => {
-    const start = text.indexOf(word.text);
-    
-    return {
-      ...word,
-      start,
-      end: start + word.text.length,
-    };
-  });
+  return words.reduce((all, word) => {
+    let start = -1;
+    while ((start = text.indexOf(word.text, start + 1)) !== -1) {
+      all.push({
+        ...word,
+        start,
+        end: start + word.text.length,
+      });
+    }
+
+    return all;
+  }, [] as WordWithPosition<Args>[]);
 }
